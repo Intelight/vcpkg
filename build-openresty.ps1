e9fe72f675718941712d6b46b71a5d3538b1bfdc
thomas-creel <137831176+thomas-creel@users.noreply.github.com>
2024-08-17 01:03:14 -0400

    lua easy crypto, not working yet

--- build-openresty.ps1
+++ build-openresty.ps1
@@ -69,6 +69,29 @@ git apply --verbose 0001-fix-link-with-vcpkg-static-openssl.patch
 ..\..\luarocks.bat make "CRYPTO_DIR=$vcpkg" "OPENSSL_DIR=$vcpkg"
 cd ..\..
 
+
+# zipwriter depends on struct, bit32, lua-zlib, luaossl
+.\luarocks.bat install lua-zlib 1.2-2 "ZLIB_DIR=$vcpkg\lib" "ZLIB_INCDIR=$vcpkg\include"
+.\luarocks.bat install bit32 5.3.5.1-1
+.\luarocks.bat install struct 1.4-1
+.\luarocks.bat install zipwriter 0.1.5-1
+
+
+.\luarocks.bat install fly-bgcrypto-sha 0.0.1-1
+.\luarocks.bat install fly-bgcrypto-pbkdf2 0.0.1-1
+.\luarocks.bat install lua-easy-crypto 1.0.0-1 # uses luossl to easily encrypt/decrypt files. not installing because need exactly luaossl 20171028-0
+
+
+# .\luarocks.bat unpack zipwriter 0.1.5-1
+# cd zipwriter-0.1.5-1
+# cp "$patches\zipwriter\*" .\
+# git init .
+# git apply --verbose 0001-replace-luacrypto-with-luaossl.patch
+# cd zipwriter
+# ..\..\luarocks.bat make
+# cd ..\..
+
+
 .\luarocks.bat unpack phpass 1.0-1
 cd phpass-1.0-1
 cp "$patches\phpass\*" .\
@@ -86,9 +109,10 @@ git apply --verbose 0001-replace-luacrypto-with-luaossl.patch
 cd luajwt
 ..\..\luarocks.bat make
 cd ..\..
-cd ..\..\..
+
 
 # Copy openresty and lua into installed dir
+cd ..\..\..
 tar xf .\openresty\zips\openresty-1.25.3.1-win64.zip --strip-components=1 -C "$openresty"
 cp -r "$lualocal\systree\lib\lua\5.1\*" "$openresty\"
 cp -r "$lualocal\systree\share\lua\5.1\*" "$openresty\lua\"
